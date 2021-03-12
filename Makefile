4978e6656efaed5268842c5f6a8a60a73234caa2
seungyel <lsy2246@naver.com>
2021-03-12 23:50:14 +0900

    ft_printf

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: seungyel <seungyel@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/03/08 21:21:01 by seungyel          #+#    #+#              #
-#    Updated: 2021/03/08 21:42:35 by seungyel         ###   ########.fr        #
+#    Updated: 2021/03/12 23:34:47 by seungyel         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,6 +14,15 @@
 NAME = libftprintf.a
 TEST = ft_printf.out
 SRCS = ft_printf.c \
+			sources/ft_putchar.c \
+			sources/ft_putnbr_fd.c \
+			sources/ft_putchar_fd.c \
+			sources/ft_strchr.c \
+			sources/ft_strdup.c \
+			sources/ft_strlen.c \
+			sources/ft_strlcpy.c \
+			sources/ft_itoa.c \
+			sources/ft_isdigit.c
 
 OBJS = $(SRCS:.c=.o)
 
@@ -24,7 +33,7 @@ $(NAME):
 	ar rc $(NAME) *.o
 
 test: $(NAME)
-	@gcc -g -L. -lftprintf $(SRCS) main.c -o $(TEST)
+	@gcc -g -L. -lftprintf $(SRCS) -o $(TEST)
 
 clean:
 	@rm -rf *.o
--- ft_printf.c
+++ ft_printf.c
@@ -1,34 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seungyel <seungyel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/12 19:52:41 by seungyel          #+#    #+#             */
+/*   Updated: 2021/03/12 23:45:27 by seungyel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 #include <stdarg.h>
-#include <string.h>
+#include "sources/ft_printf.h"
+#include <stdio.h>
 
-int ft_printf(char *args, ...)
+int	ft_printf(const char *input, ...)
 {
-	va_list ap;
-	int num;
 	int i;
+	int j;
+	int arg;
+	int char_count;
+	int min_width;
+	char *char_arg;
+	va_list	ap;
 
 	i = 0;
-	va_start(ap, args);
-
-	if (memchr(args, %) != NULL)
+	j = 0;
+	char_count = 0;
+	va_start(ap, input);
+	while (input[i] != 0)
 	{
-		if (args)
-	}
-
-	while (i < count)
-	{
-		num = va_arg(ap, int);
-		i++;
+		if (input[i] == '%')
+		{
+			while (ERROR != 0)
+			{
+				if (input[++i] == 'd')
+				{
+					i++;
+					arg = va_arg(ap, int);
+					char_arg = ft_itoa(arg);
+					while (char_arg[j] != 0)
+					{
+						ft_putchar(char_arg[j]);
+						char_count++;
+						j++;
+					}
+				}
+			}	
+			else
+			{
+				min_width = input[i];
+				char_count = char_count + min_width;
+				printf("min_width: %d", min_width);
+				i++;
+			}
+		}
+		else
+		{
+			ft_putchar(input[i]);
+			char_count++;
+			i++;
+		}
 	}
 	va_end(ap);
-
-	return (0);
+	return (char_count);
 }
 
-int main()
+int	main()
 {
-	ft_printf("%s", "안녕");
-
+	int n = ft_printf("aa%3daa\n", 10);
+	printf("%d\n", n);
 	return (0);
 }
