922af1507cbce103ee58fce9cc6edada63d3311a
seungyel <lsy2246@naver.com>
2021-03-13 01:56:33 +0900

    30%

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: seungyel <seungyel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/12 19:52:41 by seungyel          #+#    #+#             */
-/*   Updated: 2021/03/13 01:30:31 by seungyel         ###   ########.fr       */
+/*   Updated: 2021/03/13 01:55:33 by seungyel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@ int	ft_printf(const char *input, ...)
 	int arg;
 	int char_count;
 	int min_width;
+	int precision;
 	int flag_minus;
 	int flag_zero;
 	char *char_arg;
@@ -33,15 +34,16 @@ int	ft_printf(const char *input, ...)
 	char_count = 0;
 	flag_minus = 0;
 	flag_zero = 0;
+	precision = 0;
 	va_start(ap, input);
 	while (input[i] != 0)
 	{
 		if (input[i] == '%')
 		{
 			i++;
-			while (input[i] == '-' || input[i] == '0')
+			while (input[i] == '-' || input[i] == '0') // flag확인
 			{
-				if (input[i] == '-') // flag확인
+				if (input[i] == '-') 
 				{
 					flag_minus = 1;
 				}
@@ -51,32 +53,32 @@ int	ft_printf(const char *input, ...)
 				}
 				i++;
 			}
-			printf("flag_minus=%d\n", flag_minus);
-			printf("flag_zero=%d\n", flag_zero);
-			while (input[i] != 0)
+			while (ft_isdigit(input[i]) == 1) // min_width
 			{
-				if (input[i] == 'd')
+				min_width = min_width * 10 + (input[i] - '0');
+				i++;
+			}
+			if (input[i] == '.') // precision
+			{ 
+				i++;
+				while (ft_isdigit(input[i]) == 1) 
 				{
-					arg = va_arg(ap, int);
-					char_arg = ft_itoa(arg);
-					while (char_arg[j] != 0)
-					{
-						ft_putchar(char_arg[j]);
-						char_count++;
-						j++;
-					}
-					break ;
+					precision = precision * 10 + (input[i] - '0');
+					i++;
 				}
-				else
+			}
+			if (input[i] == 'd') // d 옵션 판단
+			{
+				arg = va_arg(ap, int);
+				char_arg = ft_itoa(arg);
+				while (char_arg[j] != 0)
 				{
-					if (ft_isdigit(input[i]) == 1)
-					{
-						min_width = min_width * 10 + (input[i] - '0');
-					}
+					ft_putchar(char_arg[j]);
+					char_count++;
+					j++;
 				}
 				i++;
 			}
-			printf("min_width= %d\n", min_width);
 		}
 		else
 		{
@@ -86,12 +88,16 @@ int	ft_printf(const char *input, ...)
 		}
 	}
 	va_end(ap);
+	printf("%d\n",flag_minus);
+	printf("%d\n",flag_zero);
+	printf("%d\n",min_width);
+	printf("%d\n",precision);
 	return (char_count);
 }
 
 int	main()
 {
-	int n = ft_printf("aa%-30daa\n", 10);
+	int n = ft_printf("aa%-30.120daa\n", 10);
 	printf("%d\n", n);
 	return (0);
 }
